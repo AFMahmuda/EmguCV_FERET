bd742254ffcb2a4bb64f9341e5e5df622866d550
AFMahmuda <afmahmuda@live.com>
2015-08-20 13:03:52 +0700

    additional files

---
+++ FERET_Login/Authorization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FERET_Login
+{
+    class Authorization
+    {
+        public Authorization()
+        { 
+        
+        }
+
+        public int Login()
+        {
+            return -1;
+        }
+
+        public int Register()
+        {
+            return -1;
+        }
+       
+    }
+}
---
+++ FERET_Login/FaceRecognition.cs
@@ -0,0 +1,27 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FERET_Login
+{
+    class FaceRecognition
+    {
+        public FaceRecognizer recognizer { get; set; }
+
+        public FaceRecognition()
+        {
+            
+        }
+
+        public String Recognize()
+        {
+
+            return String.Empty;
+        }
+
+
+    }
+}
